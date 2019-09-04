02e73d807cf6e2d3e7a22838a3d05d94956761f8
sathish-spidie <sathishshaj9484@gmail.com>
2019-09-04 20:09:01 +0530

    Implemented Controller for Register

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 import { RegisterView } from "./view/RegisterView";
+import { RegisterController } from "@abb/controller";
+import { FormikValues, FormikErrors } from "formik";
 
 // container -> view
 // container -> connector -> view
 //   Logic      share Logic  RN(or)R(the view is different for React and RN)
 // controller -> connector -> view
 
-export const RegisterConnector = () => {
-  const submit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
+export const RegisterConnector: React.FC = () => {
   return (
-    <div>
-      <RegisterView submit={submit} />
-    </div>
+    <RegisterController>
+      {({
+        submit
+      }: {
+        submit: () => Promise<FormikErrors<FormikValues> | null>;
+      }) => <RegisterView submit={submit} />}
+    </RegisterController>
   );
 };
